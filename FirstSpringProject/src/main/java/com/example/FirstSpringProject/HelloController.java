a08edc4747c4f695ed0c1a298f788073696bfd7d
Krishna gopal singh <krishnagopalsingh587@gmail.com>
2025-03-01 16:34:29 +0530

    Added UC4-requestWithPathAndQueryBoth

--- FirstSpringProject/src/main/java/com/example/FirstSpringProject/HelloController.java
+++ FirstSpringProject/src/main/java/com/example/FirstSpringProject/HelloController.java
@@ -30,5 +30,10 @@ public class HelloController {
          return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
      }
  
+     // Use Case 5: PUT Request with Path Variable and Query Parameter
+    @PutMapping("/put/{firstName}")
+    public String sayHelloWithPut(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
+    }
   
 }
